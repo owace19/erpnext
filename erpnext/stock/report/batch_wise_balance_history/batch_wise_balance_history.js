c064fe0cbac2ff1fc236f155f05642ab8d888167
Muhammad Owais <owais_ace@hotmail.com>
2020-01-18 17:33:41 +0500

    fix : batch wise search filter error

--- erpnext/stock/report/batch_wise_balance_history/batch_wise_balance_history.js
+++ erpnext/stock/report/batch_wise_balance_history/batch_wise_balance_history.js
@@ -1,4 +1,7 @@
-rappe.query_reports["Batch-Wise Balance History"] = {
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
+frappe.query_reports["Batch-Wise Balance History"] = {
 	"filters": [
 		{
 			"fieldname":"from_date",
@@ -46,25 +49,5 @@ rappe.query_reports["Batch-Wise Balance History"] = {
 			"options": "Batch"
 		},
 
-	]
-}// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-// License: GNU General Public License v3. See license.txt
-
-frappe.query_reports["Batch-Wise Balance History"] = {
-	"filters": [
-		{
-			"fieldname":"from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"width": "80",
-			"default": frappe.sys_defaults.year_start_date,
-		},
-		{
-			"fieldname":"to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"width": "80",
-			"default": frappe.datetime.get_today()
-		}
 	]
 }
